839a2b2c6cc78bc8ba9df86ec12fdfa59690bc85
FranklinMiranda <fdm61344@gmail.com>
2023-01-04 11:31:00 -0500

    CM

--- variables.js
+++ variables.js
@@ -14,3 +14,24 @@ console.log(multiply(num, number));
 
 let num1 = 25;
 console.log(multiply(num, num1));
+
+const name = 'Franklin';
+const lname = 'Miranda';
+
+function fullname(fname, lname) {
+  return `${fname} ${lname}`;
+}
+
+console.log(fullname(name, lname));
+
+const testStatus = false;
+
+function tester(status, num) {
+  if (status === true) {
+    return num;
+  } else {
+    return num * num;
+  }
+}
+
+console.log(tester(testStatus, num1))
