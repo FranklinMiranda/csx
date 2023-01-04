556d050ff83a1c417337e18541081b5c8d3a46d7
FranklinMiranda <fdm61344@gmail.com>
2023-01-04 11:26:18 -0500

    CM

---
+++ variables.js
@@ -0,0 +1,16 @@
+var number = 5;
+console.log(number);
+
+console.log(multiply(number, number));
+
+function multiply(x, y) {
+  return x * y;
+}
+
+console.log(multiply(number, number));
+
+const num = 10;
+console.log(multiply(num, number));
+
+let num1 = 25;
+console.log(multiply(num, num1));
