f9951f1e151c88736822b424449f04b7458a20a8
FranklinMiranda <fdm61344@gmail.com>
2023-01-04 13:38:12 -0500

    CM

--- dataTypes.js
+++ dataTypes.js
@@ -10,3 +10,22 @@ let undefined;
 // Composite Data Types include objects and arrays
 const object = {objectName : 'Franklin'}
 const array = [1,2,3,4,5]
+
+// String Concatenation
+console.log(string + object.objectName)
+
+// Template Literal 
+const sentence = `This is a example of a temple literal using ${string}`
+
+// String Properties and Methods
+const stringLength = string.length
+const stringUpper = string.toUpperCase()
+const stringLower = string.toLowerCase()
+
+// Bracket Notation for Strings
+const letter1 = string[0]
+const letterLast = string[string.length - 1]
+
+// Typeof Operator returns the data type of the variable 
+console.log(typeof(string))
+
