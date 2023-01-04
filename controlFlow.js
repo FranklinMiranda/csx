a3f3fb2f2cc86e808a91bca47e881ea18d53c884
FranklinMiranda <fdm61344@gmail.com>
2023-01-04 14:20:24 -0500

    CM

--- controlFlow.js
+++ controlFlow.js
@@ -1,11 +1,42 @@
 // Control Flow includes if statements and switch statements
 
 // If Statements use the keywords if, else if, and else
-const x = 10
-const y = 20 
-const z = 30
+const x = 10;
+const y = 20;
+const z = 30;
 
-if (z > x) {console.log('30 is greater than 10')}
-else if (y < z) {console.log('20 is less than 30')}
-else if (y + x === z) {console.log('20 + 10 is equal to 30')}
-else {console.log('None of these statements are true')}
+if (z < x) {
+  console.log('30 is greater than 10');
+} else if (y + x === z) {
+  console.log('20 + 10 is equal to 30');
+} else {
+  console.log('None of these statements are true');
+}
+
+// Switch Statements use the keywords switch, case, break, and default
+switch (new Date().getDay()) {
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+    break;
+}
+
+console.log(day);
+
--- loops.js
+++ loops.js
@@ -1,1 +1,37 @@
 // Loops include for, for in, for off, and while Loops
+
+// A For Loop uses 3 expressions to loop through a block of code
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// A For In Loop loops through the properties of an object
+const obj = { 1: 'apple', 2: 'banana', 3: 'cherry', 4: 'grape', 5: 'pineapple' };
+
+for (let fruit in obj) {
+  console.log(fruit);
+  console.log(obj[fruit]);
+}
+
+// A For of Loop loops through thevalues of an iterable object
+const arr = ['bmw', 'mercedes', 'volvo', 'chrysler', 'audi'];
+
+for (let car of arr) {
+  console.log(car);
+}
+
+// A While Loop loops through a block of code while the specified condition is true
+let i = 0; 
+while (i < 10) {
+  console.log(i);
+  i++;
+}
+
+// A Do While Loop executes code block once then continues looping while condition is true
+let x = 0;
+
+do {
+    console.log(x)
+    x++;
+} 
+while (x < 5);
