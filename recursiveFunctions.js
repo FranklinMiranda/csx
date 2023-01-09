8a42ab9407b5a28588ebb77e215ffde234546df8
FranklinMiranda <fdm61344@gmail.com>
2023-01-09 14:38:21 -0500

    CM

--- recursiveFunctions.js
+++ recursiveFunctions.js
@@ -2,6 +2,7 @@
 // Date : 1/9/2023
 // Author : Franklin Miranda
 // Description : Recursive Functions call themselves inside the function body
+// If the exit condition of a recursive function returns the result then the outer function needs to return the recursive function, else it needs to return the stored result variable
 function factorial(num) {
     // Using Closure to Store result Variable
 	let result = num;
@@ -26,6 +27,7 @@ function getLength(array) {
       if (!array[counter]) {return counter}
       return recurse (counter += 1)
     }
+    // Returning the recurse function because the result is returned from the exit condition 
     return recurse ()
   }
   
@@ -35,3 +37,37 @@ function getLength(array) {
   console.log(getLength([1, 2, 3, 4, 5])); // -> 5
   console.log(getLength([])); // -> 0
   
+  // Recursive Function POW
+  function pow(base, exponent) {
+    // Recursive Function with result default parameter set to result
+	function recurse (result = base) {
+        // Returning result from the exit condition
+    if (exponent === 1) {return result}
+    exponent--
+    return recurse (result *= base)
+  }
+  // Returning recursive function from outer function because the exit condition returns result 
+  return recurse ()
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(pow(2, 4)); // -> 16
+console.log(pow(3, 5)); // -> 243
+
+// Recursive FLOW function
+function flow(input, funcArray) {
+	function recurse (counter = 0) {
+    if (counter === funcArray.length) {return input}
+    input = funcArray[counter](input)
+    return recurse (counter += 1)
+  }
+  return recurse()
+}
+
+// To check if you've completed the challenge, uncomment this code!
+function multiplyBy2(num) { return num * 2; }
+function add7(num) { return num + 7; }
+function modulo4(num) { return num % 4; }
+function subtract10(num) { return num - 10; }
+const arrayOfFunctions = [multiplyBy2, add7, modulo4, subtract10];
+console.log(flow(2, arrayOfFunctions)); // -> -7
